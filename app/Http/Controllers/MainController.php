2f08db5c2ed53ac676d331b74493bd9525185f1b
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2024-10-06 17:24:10 -0300

    feat: add show questions

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class MainController extends Controller
 {
-    private $appData;
+    private array $appData;
 
     public function __construct()
     {
@@ -14,8 +16,86 @@ class MainController extends Controller
         $this->appData = require(app_path('appData.php'));
     }
 
-    public function index()
+    public function index(): View
     {
-        return response()->json($this->appData);
+        return view('home');
     }
+
+    public function submit(Request $request): RedirectResponse
+    {
+        $request->validate(
+            [
+                'total_questions' => 'required|integer|min:3|max:30',
+            ],
+            [
+                'total_questions.required' => 'O número de questões é obrigatorio',
+                'total_questions.integer' => 'O número de questões tem que ser um valor inteiro',
+                'total_questions.min' => 'No mínimo :min questões',
+                'total_questions.max' => 'No máximo :max questões'
+            ]
+        );
+
+        $totalQuestions = intval($request->get('total_questions'));
+
+        $quiz = $this->prepareQuiz($totalQuestions);
+
+        session()->put([
+            'quiz' => $quiz,
+            'total_questions' => $totalQuestions,
+            'current_question' => 1,
+            'correct_answers' => 0,
+            'wrong_answers' => 0,
+        ]);
+
+        return redirect()->route('game');
+    }
+
+    public function game(): View
+    {
+        $quiz = session()->get('quiz');
+        $totalQuestions = session()->get('total_questions');
+        $currentQuestion = session()->get('current_question') - 1;
+
+        $answers = $quiz[$currentQuestion]['wrong_answers'];
+        $answers[] = $quiz[$currentQuestion]['capital'];
+
+        shuffle($answers);
+
+        return view('game', [
+            'country' => $quiz[$currentQuestion]['country'],
+            'totalQuestions' => $totalQuestions,
+            'currentQuestion' => $currentQuestion,
+            'answers' => $answers,
+        ]);
+    }
+
+    private function prepareQuiz(int $totalQuestions): array
+    {
+        $questions = [];
+        $totalCountries = count($this->appData);
+
+        $indexes = range(0, $totalCountries - 1);
+        shuffle($indexes);
+        $indexes = array_slice($indexes, 0, $totalQuestions);
+
+        $questionsNumber = 1;
+        foreach ($indexes as $index) {
+            $question['question_number'] = $questionsNumber++;
+            $question['country'] = $this->appData[$index]['country'];
+            $question['capital'] = $this->appData[$index]['capital'];
+
+            // wrong answers
+            $other_capitals = array_column($this->appData, 'capital');
+            $other_capitals = array_diff($other_capitals, [$question['capital']]);
+            shuffle($other_capitals);
+            $question['wrong_answers'] = array_slice($other_capitals, 0, 3);
+
+            $question['correct_answer'] = null;
+
+            $questions[] = $question;
+        }
+
+        return $questions;
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home');
-
-Route::get('/show', [MainController::class, 'index']);
+Route::get('/', [MainController::class, 'index'])->name('home');
+Route::post('/', [MainController::class, 'submit'])->name('submit');
+Route::get('/game', [MainController::class, 'game'])->name('game');
